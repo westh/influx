3d08c6189f36bfeb71b31545a95385c3d2ee8b0c
Westh <haakonwesth@gmail.com>
2020-12-14 07:16:05 +0100

    fix: 🐛 transpilling hell

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-module.exports = require('./dist')
+import axios from 'axios'
+
+let influx = null
+
+function init ({ url, org, bucket, token }) {
+  influx = axios.create({
+    baseURL: `${url}/api/v2/write?org=${org}&bucket=${bucket}`,
+    headers: { Authorization: `Token ${token}` }
+  })
+}
+
+function insert ({ measurementName, host, data }) {
+  const isInfluxInitiated = !!influx
+  if (!isInfluxInitiated) throw new Error('Influx needs to be initiated')
+
+  const fields = Object.entries(data)
+    .map(entry => `${entry[0]}=${entry[1]}`)
+    .join(',')
+  return influx.post('', `${measurementName},host=${host} ${fields}`)
+}
+
+async function insertSync ({ measurementName, host, data }) {
+  await insert({ measurementName, host, data })
+}
+
+export default { init, insert, insertSync }
